ab48f984deda92ea2575384b7b66235a028a1473
HAK1580 <aliofficial1580630@gmail.com>
2026-09-04 23:06:09 +0500

    updated the ui

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // 1. Environment variable se Base URL pick karein with Fallback
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL||'http://localhost:3000';
 
 
 // 2. Axios Instance create karein
